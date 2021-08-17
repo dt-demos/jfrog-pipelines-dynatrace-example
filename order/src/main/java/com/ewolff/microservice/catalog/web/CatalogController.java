572d90901528013d67d085439a117632ce5501c4
robertjahn <rob.jahn@gmail.com>
2021-08-17 08:08:00 -0400

    adjust from order1

---
+++ order/src/main/java/com/ewolff/microservice/catalog/web/CatalogController.java
@@ -0,0 +1,136 @@
+package com.ewolff.microservice.catalog.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Calendar;
+import java.util.Date; 
+import java.io.*;
+
+import com.ewolff.microservice.catalog.Item;
+import com.ewolff.microservice.catalog.ItemRepository;
+
+
+@Controller
+public class CatalogController {
+
+	private String version;
+
+	private final ItemRepository itemRepository;
+
+	private String getVersion() {
+		System.out.println("Current APP_VERSION: " + this.version);
+		return this.version;
+	}
+
+	private void setVersion(String newVersion) {
+		this.version = newVersion;
+		System.out.println("Setting APP_VERSION to: " + this.version);
+	}
+
+	@Autowired
+	public CatalogController(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+		this.version = System.getenv("APP_VERSION");
+	}
+
+	@RequestMapping(value = "/{id}.html", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public ModelAndView Item(@PathVariable("id") long id) {
+		return new ModelAndView("item", "item", itemRepository.findById(id).get());
+	}
+
+	@RequestMapping("/list.html")
+	public ModelAndView ItemList() {
+		return new ModelAndView("itemlist", "items", itemRepository.findAll());
+	}
+
+	@RequestMapping(value = "/form.html", method = RequestMethod.GET)
+	public ModelAndView add() {
+		return new ModelAndView("item", "item", new Item());
+	}
+
+	@RequestMapping(value = "/form.html", method = RequestMethod.POST)
+	public ModelAndView post(Item Item) {
+		Item = itemRepository.save(Item);
+		return new ModelAndView("success");
+	}
+
+	@RequestMapping(value = "/{id}.html", method = RequestMethod.PUT)
+	public ModelAndView put(@PathVariable("id") long id, Item item) {
+		item.setId(id);
+		itemRepository.save(item);
+		return new ModelAndView("success");
+	}
+
+	@RequestMapping(value = "/searchForm.html", produces = MediaType.TEXT_HTML_VALUE)
+	public ModelAndView searchForm() {
+		return new ModelAndView("searchForm");
+	}
+
+	@RequestMapping(value = "/searchByName.html", produces = MediaType.TEXT_HTML_VALUE)
+	public ModelAndView search(@RequestParam("query") String query) {
+		return new ModelAndView("itemlist", "items",
+				itemRepository.findByNameContaining(query));
+	}
+
+	@RequestMapping(value = "/{id}.html", method = RequestMethod.DELETE)
+	public ModelAndView delete(@PathVariable("id") long id) {
+		itemRepository.deleteById(id);
+		return new ModelAndView("success");
+	}
+
+   @RequestMapping(value = "/version", method = RequestMethod.GET)
+   @ResponseBody
+   public String showVersion() {
+		String version;
+		try {
+			version = this.getVersion();
+		}
+		catch(Exception e) {
+			version = "APP_VERSION not found";
+		}
+		return version;
+   } 
+
+	@RequestMapping(value = "setversion/{version}", method = RequestMethod.GET)
+	public ModelAndView webSetVersion(@PathVariable("version") String newVersion) {
+		this.setVersion(newVersion);
+		return new ModelAndView("success");
+	}
+	
+	@RequestMapping(value = "/manifest", method = RequestMethod.GET)
+	@ResponseBody
+	public String getManifest() {
+		 File file = new File("/MANIFEST"); 
+		 String manifest = "MANIFEST file not found";
+		 try {
+			 BufferedReader br = new BufferedReader(new FileReader(file));
+			 String line = br.readLine();
+			 manifest = line + "<BR>";
+			 while ((line = br.readLine()) != null) {
+				manifest = manifest + line + "<BR>";
+			 }
+			 br.close();
+		 }
+		 catch(Exception e) {
+			manifest = e.getMessage();
+		 }
+		 return manifest;
+	}
+
+	@RequestMapping(value = "/health", method = RequestMethod.GET)
+	@ResponseBody
+	public String getHealth() {
+
+		Date dateNow = Calendar.getInstance().getTime();
+		String health = "{ \"health\":[{\"service\":\"catalog-service\",\"status\":\"OK\",\"date\":\"" + dateNow + "\" }]}";
+		return health;
+	}
+}
