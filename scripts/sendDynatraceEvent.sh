4b36f4a0446c6bb49c163312ee08061fdd30397c
robertjahn <rob.jahn@gmail.com>
2021-08-20 16:45:19 -0400

    adjust send dtevent

--- scripts/sendDynatraceEvent.sh
+++ scripts/sendDynatraceEvent.sh
@@ -6,63 +6,61 @@
 
 DT_BASEURL=$1
 DT_API_TOKEN=$2
-DT_TAG_PROJECT=$3
-DT_TAG_STAGE=$4
-DT_TAG_SERVICE=$5
-DEPLOYMENT_VERSION=$6
-CI_BACK_LINK=$7
-SOURCE=$8
+DEPLOYMENT_PROJECT=$3
+DEPLOYMENT_VERSION=$4
+CI_BACK_LINK=$5
+SOURCE=$6
+IMAGE_NAME=$7
+HELM_CHART=$8
+JFROG_USER=$9
+JFROG_ARTIFACTORY_URL=${10}
+PIPELINE_REPO=${11}
+JFROG_PIPELINE_NAME=${12}
+JFROG_RUN_NUMBER=${13}
+JFROG_PROJECT=${14}
+TAG_RULE="${15}"
 
 DT_TAG_PROJECT=$DT_TAG_PROJECT
 DT_API_URL="$DT_BASEURL/api/v1/events"
-DEPLOYMENT_NAME="Set $DT_TAG_SERVICE to version $DEPLOYMENT_VERSION"
+DEPLOYMENT_NAME="Set $DEPLOYMENT_PROJECT to version $DEPLOYMENT_VERSION"
 
 echo "================================================================="
 echo "Sending Dynatrace Deployment event"
 echo "DT_API_URL          = $DT_API_URL"
 echo "DEPLOYMENT_NAME     = $DEPLOYMENT_NAME"
 echo "DEPLOYMENT_VERSION  = $DEPLOYMENT_VERSION"
-echo "DT_TAG_PROJECT      = $DT_TAG_PROJECT"
-echo "DT_TAG_SERVICE      = $DT_TAG_SERVICE"
-echo "DT_TAG_STAGE        = $DT_TAG_STAGE"
 echo "CI_BACK_LINK        = $CI_BACK_LINK"
 echo "SOURCE              = $SOURCE"
+echo "IMAGE_NAME          = $IMAGE_NAME"
+echo "HELM_CHART          = $HELM_CHART"
+echo "JFROG_USER          = $JFROG_USER"
+echo "JFROG_ARTIFACTORY_URL=$JFROG_ARTIFACTORY_URL"
+echo "PIPELINE_REPO       = $PIPELINE_REPO"
+echo "JFROG_PIPELINE_NAME = $JFROG_PIPELINE_NAME"
+echo "JFROG_RUN_NUMBER    = $JFROG_RUN_NUMBER"
+echo "JFROG_PROJECT       = $JFROG_PROJECT"
+echo "TAG_RULE            = $TAG_RULE"
 echo "================================================================="
 POST_DATA=$(cat <<EOF
   {
       "eventType" : "CUSTOM_DEPLOYMENT",
       "source" : "$SOURCE" ,
       "deploymentName" : "$DEPLOYMENT_NAME",
-      "deploymentVersion" : "$DEPLOYMENT_VERSION"  ,
-      "deploymentProject" : "$DT_TAG_PROJECT" ,
+      "deploymentVersion" : "$DEPLOYMENT_VERSION",
+      "deploymentProject" : "$JFROG_PROJECT" ,
       "ciBackLink" : "$CI_BACK_LINK",
       "customProperties": {
-          "Example Custom Property 1" : "Example Custom Value 1",
-          "Example Custom Property 2" : "Example Custom Value 2",
-          "Example Custom Property 3" : "Example Custom Value 3"
+          "JFrog User" : "$JFROG_USER",
+          "JFrog Artifactory URL" : "$JFROG_ARTIFACTORY_URL",
+          "JFrog Project" : "$JFROG_PROJECT",
+          "JFrog Pipeline Name" : "$JFROG_PIPELINE_NAME",
+          "JFrog Run Number" : "$JFROG_RUN_NUMBER",
+          "JFrog Pipeline Repo" : "$PIPELINE_REPO",
+          "Image Name" : "$IMAGE_NAME",
+          "Helm Chart" : "$HELM_CHART"
       },
       "attachRules" : {
-            "tagRule" : [
-                {
-                  "meTypes":["PROCESS_GROUP_INSTANCE"],
-                  "tags": [
-                      {
-                            "context": "CONTEXTLESS",
-                            "key": "service",
-                              "value": "$DT_TAG_SERVICE"
-                      },
-                          {
-                            "context": "CONTEXTLESS",
-                            "key": "project",
-                              "value": "$DT_TAG_PROJECT"
-                      },
-                          {
-                            "context": "CONTEXTLESS",
-                            "key": "stage",
-                              "value": "$DT_TAG_STAGE"
-                      }
-            ]}
-          ]}
+            "tagRule" : ${TAG_RULE}
         }
   }
 EOF
